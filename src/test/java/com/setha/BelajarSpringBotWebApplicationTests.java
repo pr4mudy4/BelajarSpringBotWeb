78e6a2d50b530e971a9596eef6927c2ceafbca68
setha <setha.sakti@iconpln.co.id>
2018-09-27 18:54:36 +0700

    belajar springboot web

---
+++ src/test/java/com/setha/BelajarSpringBotWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.setha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BelajarSpringBotWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
